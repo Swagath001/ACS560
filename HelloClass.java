c2c723a1e5ab0e51a4e4be185de10b1eda80a2b1
Swagath Bollineni <swagath.bollineni@gmail.com>
2014-09-14 16:24:00 -0400

    first commit

---
+++ HelloClass.java
@@ -0,0 +1,17 @@
+package helloPackage;
+
+public class HelloClass {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Hello ACS560!");
+		  
+		// Source : http://www.javaprogrammingforums.com/java-jdk-ide-tutorials/253-beginners-eclipse-tutorial-how-run-first-java-application-eclipse.html
+
+	}
+
+}
